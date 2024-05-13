b353887f297b5d8f45c6c6e71bace4487e933b71
sprajapati123 <sandisprajapati52@gmail.com>
2024-05-13 13:08:52 +0545

    splash

---
+++ app/src/main/java/com/example/a34b_splashandbuttonnavigation/ui/theme/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.a34b_splashandbuttonnavigation.ui.theme
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.a34b_splashandbuttonnavigation.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+              var intent = Intent(this@MainActivity,
+                  DashBoardActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        },3000)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
